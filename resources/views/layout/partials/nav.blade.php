cd42135573410980a47e6703c451daacafef51d0
Ostin Drais <ostin@nationalservicealliance.com>
2021-10-08 09:17:12 -0500

    Fix mobile navigation

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -3,7 +3,7 @@
         <a href="/" class="navbar-brand">
             <img src="/images/thegig_badge.png" width="100" height="90" />
             The GIG USA</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
